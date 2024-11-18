0a528b0646bace002195dba5f08239ff2da6603b
shajoezhu <sha.joe.zhu@gmail.com>
2024-11-18 23:50:03 +0800

    rm comment

--- tests/testthat/test-h_tidy_pool.R
+++ tests/testthat/test-h_tidy_pool.R
@@ -5,7 +5,6 @@ testthat::test_that("h_tidy_pool is produced correctly", {
   result <- h_tidy_pool(dat$pars[1:3])[,-1]
 
   expected <- data.frame(
-    # c("ref", "alt"),
     c("-1.61581995766697", "-1.70762640404516"),
     c("0.486231596928312", "0.474957263044525"),
     c("-2.57577141468279", "-2.64531930504159"),
@@ -20,7 +19,6 @@ testthat::test_that("h_tidy_pool is produced correctly", {
   ) %>% dplyr::mutate(dplyr::across(everything(), as.numeric))
 
   colnames(expected) <- c(
-    # "group",
     "est", "se_est", "lower_cl_est", "upper_cl_est", "est_contr", "se_contr",
     "lower_cl_contr", "upper_cl_contr", "p_value", "relative_reduc"
   )

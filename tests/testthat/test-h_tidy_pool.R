d2bf4a15a6f8b83f1cbbf67d6a3ba4f70ebb43be
shajoezhu <sha.joe.zhu@gmail.com>
2024-11-18 22:29:13 +0800

    update

--- tests/testthat/test-h_tidy_pool.R
+++ tests/testthat/test-h_tidy_pool.R
@@ -2,11 +2,10 @@ data(rbmi_test_data)
 dat <- rbmi_test_data
 
 testthat::test_that("h_tidy_pool is produced correctly", {
-  result <- h_tidy_pool(dat$pars[1:3]) %>%
-    dplyr::mutate(dplyr::across(everything(), as.character))
+  result <- h_tidy_pool(dat$pars[1:3])[,-1]
 
   expected <- data.frame(
-    c("ref", "alt"),
+    # c("ref", "alt"),
     c("-1.61581995766697", "-1.70762640404516"),
     c("0.486231596928312", "0.474957263044525"),
     c("-2.57577141468279", "-2.64531930504159"),
@@ -18,10 +17,11 @@ testthat::test_that("h_tidy_pool is produced correctly", {
     c(NA, "0.893177242657404"),
     c(NA, "0.056817249930957"),
     stringsAsFactors = FALSE
-  )
+  ) %>% dplyr::mutate(dplyr::across(everything(), as.numeric))
 
   colnames(expected) <- c(
-    "group", "est", "se_est", "lower_cl_est", "upper_cl_est", "est_contr", "se_contr",
+    # "group",
+    "est", "se_est", "lower_cl_est", "upper_cl_est", "est_contr", "se_contr",
     "lower_cl_contr", "upper_cl_contr", "p_value", "relative_reduc"
   )
   testthat::expect_identical(result, expected, tolerance = 0.000001)
